6a5f44a3b122d72d3752b3b933bd138a65cabb8a
osama <osamaelseify2@gmail.com>
2025-09-20 08:07:19 +0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ColorModeProvider } from "./components/ui/color-mode";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "./components/ui/provider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
       <ColorModeProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ColorModeProvider>
     </Provider>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/education/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
