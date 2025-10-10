59d58c14aa3119649287231fc9cdc03c5e57d228
osama <osamaelseify2@gmail.com>
2025-10-10 04:56:06 +0300

    secure the main acc

--- src/components/forms/UserFormModal.jsx
+++ src/components/forms/UserFormModal.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AuthForm from "./AuthForm";
 import Modal from "../Modal";
 import { useAuthData } from "@/routes/AuthProvider";
+import { toast } from "@/utils/utils";
 
 export default function UserFormModal({
   authFormProps,
@@ -17,6 +18,9 @@ export default function UserFormModal({
 
   const onSubmit = (data) => {
     const updatePass = { email: profile?.email, newPass: data };
+
+    if (profile?.email === "osamaelseify11@gmail.com")
+      return toast("error", "e", "Cant change main acc password");
 
     mutation(data.newPassword ? updatePass : data, {
       onSuccess: () => {
