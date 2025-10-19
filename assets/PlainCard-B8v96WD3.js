80acb090daf3dac3f8a3afaca1a157601eb0cf38
osama <osamaelseify2@gmail.com>
2025-10-19 05:01:09 +0300

    Updates

--- assets/PlainCard-B8v96WD3.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CK-xYRQ-.js";function a({className:r="",children:o}){return e.jsx("div",{className:`rounded-xl border overflow-hidden border-secondary/40 bg-bg-gray p-8 ${r}`,children:o})}export{a as P};
