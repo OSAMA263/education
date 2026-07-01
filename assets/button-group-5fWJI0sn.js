62334f0f7dc8c2d7569d42682c4d8081ca959fef
OSAMA263 <osama.khaled0707@gmail.com>
2026-07-02 02:13:27 +0300

    Updates

---
+++ assets/button-group-5fWJI0sn.js
@@ -0,0 +1,1 @@
+import{r as t,bq as p,j as s,br as u}from"./index-DN1aG0T-.js";import{G as i}from"./group-Ct8mDGSA.js";const f=t.forwardRef(function(r,e){const o=p({key:"button"}),[n,a]=t.useMemo(()=>o.splitVariantProps(r),[r,o]);return s.jsx(u,{value:n,children:s.jsx(i,{ref:e,...a})})});export{f as B};
